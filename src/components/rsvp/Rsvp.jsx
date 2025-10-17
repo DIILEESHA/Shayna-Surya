54d83f04c69b3af3910489acad254dfa90d87d11
DIILEESHA <dileeshanawarathna18@gmail.com>
2025-10-18 02:03:31 +0530

    chnage header img

--- src/components/rsvp/Rsvp.jsx
+++ src/components/rsvp/Rsvp.jsx
@@ -6,7 +6,7 @@ const Rsvp = () => {
       <p className="rsvp_title">SAVE THE DATE & SEE YOU THERE!</p>
 
       <button className="rsvps_btn">
-        <a href="https://forms.gle/godUWV1aFKE8sXnp6" target="_blank">
+        <a className="pahara" href="https://forms.gle/godUWV1aFKE8sXnp6" target="_blank">
           RSVP by 11.25.2025
         </a>
       </button>
